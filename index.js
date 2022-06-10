5d8ae349ea7ffd87f423c8db175b5b420ee370a0
陈坚镕 <2440398853@qq.com>
2022-06-10 22:15:37 +0800

    完成题目

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
-var name = 'hello';
+var name = "hello";
 // TODO write your code here...
-
+name = name.toUpperCase();
+console.log(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
+var sentence = "good afternoon, mr mike.";
 //TODO write your code here...
-
+var newStr = sentence.split(" ");
+for (var i = 0; i < newStr.length; i++) {
+  newStr[i] =
+    newStr[i].slice(0, 1).toUpperCase() + newStr[i].slice(1).toLowerCase();
+}
+sentence = newStr.join(" ");
+console.log(sentence);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
-var money = '￥20';
+var money = "￥20";
 //TODO write your code here...
+console.log(money.slice(1));
